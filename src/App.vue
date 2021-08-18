8848ee697a4dacf49939fe0bca5f9c2856c2fcf6
shaohai.li <shaohai.li@matrx.team>
2021-08-18 19:35:05 +0800

    feat: add canvas

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,107 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <canvas id="my_canvas"></canvas>
 </template>
+<script>
+export default {
+    name: 'App',
+    data() {
+        return {};
+    },
+    mounted() {
+        this.canvas = document.getElementById('my_canvas');
+        this.ctx = this.canvas.getContext('2d');
 
+        this.canvas.height = window.innerHeight;
+        this.canvas.width = window.innerWidth;
+
+        const fontSize = 16;
+
+        const columns = this.canvas.width / fontSize;
+
+        const drops = [];
+        for (let x = 0; x < columns; x++) {
+            drops[x] = 1;
+        }
+        this.drops = drops;
+        this.fontSize = fontSize;
+        // const draw = () => {
+        //     const canvas = this.canvas;
+        //     const ctx = this.ctx;
+        //     const drops = this.drops;
+        //     const fontSize = this.fontSize;
+        //     ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
+        //     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        //     ctx.fillStyle = '#0F0'; // green text
+        //     ctx.font = fontSize + 'px arial';
+        //     for (let i = 0; i < drops.length; i++) {
+        //         const text = txts[Math.floor(Math.random() * txts.length)];
+        //         ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+        //         if (drops[i] * fontSize > canvas.height || Math.random() > 0.95) drops[i] = 0;
+        //         drops[i]++;
+        //     }
+        // };
+        this.intervalId = setInterval(this.draw, 33);
+
+        window.addEventListener('resize', this.onResize);
+    },
+    beforeUnmount() {
+        window.removeEventListener('resize', this.onResize);
+    },
+    methods: {
+        draw() {
+            const canvas = this.canvas;
+            const ctx = this.ctx;
+            const drops = this.drops;
+            const fontSize = this.fontSize;
+            let txts = '01';
+            txts = txts.split('');
+            ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+            ctx.fillStyle = '#0F0'; // green text
+            ctx.font = fontSize + 'px arial';
+            for (let i = 0; i < drops.length; i++) {
+                const text = txts[Math.floor(Math.random() * txts.length)];
+                ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+                if (drops[i] * fontSize > canvas.height || Math.random() > 0.95) drops[i] = 0;
+                drops[i]++;
+            }
+        },
+        onResize() {
+            clearInterval(this.intervalId);
+            this.canvas.height = window.innerHeight;
+            this.canvas.width = window.innerWidth;
+            const fontSize = 16;
+
+            const columns = this.canvas.width / fontSize;
+
+            const drops = [];
+            for (let x = 0; x < columns; x++) {
+                drops[x] = 1;
+            }
+            this.drops = drops;
+            this.fontSize = fontSize;
+            this.intervalId = setInterval(this.draw, 33);
+        }
+    }
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+html,
+body {
+    padding: 0;
+    margin: 0;
 }
-
-#nav {
-  padding: 30px;
+html,
+body {
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+#my_canvas {
+    height: 100%;
+    width: 100%;
+    overflow: hidden;
 }
 </style>
